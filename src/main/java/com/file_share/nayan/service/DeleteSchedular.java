2cf036a56955516224055ab40bddbc80d8b516fc
Nayan Jain <71127472+NayanJain7@users.noreply.github.com>
2021-04-15 19:58:37 +0530

    Update DeleteSchedular.java

--- src/main/java/com/file_share/nayan/service/DeleteSchedular.java
+++ src/main/java/com/file_share/nayan/service/DeleteSchedular.java
@@ -19,7 +19,7 @@ public class DeleteSchedular {
 	private FileRepository fileRepo;
 	
 	// time is in seconds that is 24 hour 
-	@Scheduled(fixedDelay = 24 * 60 * 60 * 1000)
+	@Scheduled(fixedDelay = 24 * 60 * 60 * 1000 * 10 )
 	public void deleteFile() {
 	
 		
